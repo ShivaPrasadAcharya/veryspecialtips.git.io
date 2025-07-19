b134800f836278899212c1cea1a5acb70275394a
ShivaPrasadAcharya <shivaprasadacharya000000000000@gmail.com>
2025-07-19 15:17:40 +0545

    updates

--- data2Images.js
+++ data2Images.js
@@ -1,11 +1,9 @@
 // data2Images.js - Images Dataset
 var data2Images = `ID,Title,Description,Image
-001,Handbook on Good Treaty Practice,Jill Barret/Robert Beckham,Handbook.jpg
-002,Modern Treaty Law and Practice,Anthony Aust,Moderntreatylaw.jpg
-003, Negotiation,Roy J. Lewicki,Negotiation.jpg
-004, Oxford Guide to Treaties,2nd edition,Oxfordguide.jpg
-005, Group Photo,Closing Ceromony, groupphoto.jpg
-006, Shiva's Photo, Closing Ceromony, myphoto.jpg`;
+004, Policy Gap,Fig:Policy Gap,PolicyGap.png
+005, Aid Gap,Fig: Aid Gap, AidGap.png
+006, Shiva's Photo, Closing Ceromony, myphoto.jpg
+007, Group Photo, Closing Ceromony, groupphoto.jpg`;
 
 // Dataset metadata
 var data2ImagesInfo = {
