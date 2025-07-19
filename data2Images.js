1d52b5ad151e6b48abfd342c685bdde0ea2c58b0
ShivaPrasadAcharya <shivaprasadacharya000000000000@gmail.com>
2025-07-19 15:23:41 +0545

    Update data2Images.js

--- data2Images.js
+++ data2Images.js
@@ -1,9 +1,9 @@
 // data2Images.js - Images Dataset
 var data2Images = `ID,Title,Description,Image
-004, Policy Gap,Fig:Policy Gap,PolicyGap.jpg
-005,Aid Gap,Fig: Aid Gap, AidGap.jpg
+004, Policy Gap,Fig:Policy Gap,PolicyGap.png
+005,Aid Gap,Fig: Aid Gap, AidGap.png
 006, Shiva's Photo, Closing Ceromony, myphoto.jpg
-006, Group Photo, Closing Ceromony, groupphoto.jpg`;
+007, Group Photo, Closing Ceromony, groupphoto.jpg`;
 
 // Dataset metadata
 var data2ImagesInfo = {
