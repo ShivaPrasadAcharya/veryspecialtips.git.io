125dbe458017cb8f6b4e105e43de7c2a60c2ea37
ShivaPrasadAcharya <shivaprasadacharya000000000000@gmail.com>
2025-07-19 12:29:16 +0545

    updateddata4Links

--- data4Links.js
+++ data4Links.js
@@ -1,9 +1,13 @@
 // data4Links.js - Links Dataset
 var data4Links = `ID,Subject,Remark,Link
 003a,Special Tips,Officer PhoneNo,https://github.com/ShivaPrasadAcharya/veryspecialtips.git.io/edit/main/data1Officers.js
-003b,Special Tips,Images,https://github.com/ShivaPrasadAcharya/veryspecialtips.git.io/edit/main/data2Images.js
-003c,Special Tips,git-Links,https://github.com/ShivaPrasadAcharya/veryspecialtips.git.io/edit/main/data3Links.js
-003d,Special Tips,doc-Links,https://github.com/ShivaPrasadAcharya/veryspecialtips.git.io/edit/main/data4Links.js
+003b,Special Tips,git Link-Images,https://github.com/ShivaPrasadAcharya/veryspecialtips.git.io/edit/main/data2Images.js
+003c,Special Tips,git Links-Docx,https://github.com/ShivaPrasadAcharya/veryspecialtips.git.io/edit/main/data3Links.js
+003d,Special Tips,git Links-Links,https://github.com/ShivaPrasadAcharya/veryspecialtips.git.io/edit/main/data4Links.js
+003e,Special Tips,git Links-app1,https://github.com/ShivaPrasadAcharya/veryspecialtips.git.io/edit/main/app1.js
+003f,Special Tips,git Links-app2,https://github.com/ShivaPrasadAcharya/veryspecialtips.git.io/edit/main/app2.js
+003g,Special Tips,git link-app3,https://github.com/ShivaPrasadAcharya/veryspecialtips.git.io/edit/main/app3.js
+003h,Special Tips,git Links-app4,https://github.com/ShivaPrasadAcharya/veryspecialtips.git.io/edit/main/app4.js
 
  `;
 // Dataset metadata
