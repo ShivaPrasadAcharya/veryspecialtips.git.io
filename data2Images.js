f20a39da270a3f2093d644c827821f505f69f490
ShivaPrasadAcharya <shivaprasadacharya000000000000@gmail.com>
2025-07-20 19:56:01 +0545

    updates

--- data2Images.js
+++ data2Images.js
@@ -1,6 +1,7 @@
 // data2Images.js - Images Dataset
 var data2Images = `ID,Title,Description,Image
 001,SambaSadaShiva,साम्बसदाशिव, sambasadashiva.jpg
+002,SCGroupPhoto,WE, SCGroupPhoto.jpg
 004,Fig:Policy Gap,Policy Gap,PolicyGap.png
 005,Fig: Aid Gap, Aid Gap,AidGap.png
 006, Shiva's Photo, Closing Ceromony, myphoto.jpg
